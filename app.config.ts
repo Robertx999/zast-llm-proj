ccfe0a4e689927fe488dbb544a0a8903a9f33937
Robertx999 <robert.e4200@gmail.com>
2024-11-07 20:17:00 +0100

    Typst file compilation moved to command

--- app.config.ts
+++ app.config.ts
@@ -7,29 +7,6 @@ import os from "node:os";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compileTypst = async () => {
-  exec(
-    `"bin/typst${
-      os.platform() == "win32" ? ".exe" : ""
-    }" compile src/assets/typst/documentation/main.typ src/assets/typst/documentation/main.svg`,
-    (err, stdout, stderr) => {
-      // the *entire* stdout and stderr (buffered)
-      stdout && console.log(stdout);
-      stderr && console.error(stderr);
-    }
-  );
-  exec(
-    `"bin/typst${
-      os.platform() == "win32" ? ".exe" : ""
-    }" compile src/assets/typst/documentation/main.typ src/assets/typst/documentation/main.pdf`,
-    (err, stdout, stderr) => {
-      // the *entire* stdout and stderr (buffered)
-      stdout && console.log(stdout);
-      stderr && console.error(stderr);
-    }
-  );
-};
-
 export default defineConfig({
   vite: {
     resolve: {
@@ -37,12 +14,5 @@ export default defineConfig({
         "~": resolve(__dirname, "./src"),
       },
     },
-    plugins: [
-      {
-        name: "compile typst",
-        // handleHotUpdate: compileTypst,
-        buildStart: compileTypst,
-      },
-    ],
   },
 });
